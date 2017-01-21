a3b259e173cfcfd95e17f64bedaeb8fce35f6e35
AGREG124036\Alex <alegrego@uat.edu>
2017-01-20 21:15:40 -0700

    Adding the GameManager script and creating a singleton of it named _GAMEMANAGER

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+
+	public static GameManager _GAMEMANAGER = null; 
+
+	void Awake()
+	{
+		//Check if instance already exists
+		if (_GAMEMANAGER == null)
+
+			//if not, set instance to this
+			_GAMEMANAGER = this;
+
+		//If instance already exists and it's not this:
+		else if (_GAMEMANAGER != this)
+
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+	// Use this for initialization
+	}
+
+
+		void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
