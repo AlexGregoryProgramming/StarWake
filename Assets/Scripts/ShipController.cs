f6766d7f644a2ce5e1e96e9c90d7ecd13735169c
AGREG124036\Alex <alegrego@uat.edu>
2017-01-21 11:34:09 -0700

    modified movement code

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -2,12 +2,14 @@
 
 public class ShipController : MonoBehaviour
 {
+	
     public float movementSpeed = 1.5f;
 
     public float rotationSpeed = 3.0f;
 
     private float lastHeading = 0.0f;
-
+	[SerializeField]
+	private float heading;
     string LeftStickHorizontalAxis = "P1LeftStickHorizontal";
     string LeftStickVerticalAxis = "P1LeftStickVertical";
 
@@ -17,20 +19,31 @@ public class ShipController : MonoBehaviour
     /// </summary>
     void Start()
     {
+		float lh = Input.GetAxis (LeftStickHorizontalAxis);
+		float lv = Input.GetAxis (LeftStickVerticalAxis);
+		heading = Mathf.Atan2 (-lh, lv);
+		Quaternion targetRotation = Quaternion.Euler (0f, 0f, heading * Mathf.Rad2Deg);
+
         transform.forward = Vector3.up;
+		transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
     }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update()
-    {
-        float lh = Input.GetAxis(LeftStickHorizontalAxis);
-        float lv = Input.GetAxis(LeftStickVerticalAxis);
-        float heading = Mathf.Atan2(-lh, lv);
-        Quaternion targetRotation = Quaternion.Euler(0f, 0f, heading * Mathf.Rad2Deg);
+	{
+		float lh = Input.GetAxis (LeftStickHorizontalAxis);
+		float lv = Input.GetAxis (LeftStickVerticalAxis);
+		if (lh != 0 || lv != 0) 
+		{
+			heading = Mathf.Atan2 (-lh, lv);
+		}
+		Quaternion targetRotation = Quaternion.Euler (0f, 0f, heading * Mathf.Rad2Deg);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+
+		transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         transform.position += transform.up * movementSpeed * Time.deltaTime;
     }
 }
