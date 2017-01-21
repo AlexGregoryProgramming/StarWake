fe10b2294b8a1d99461b86b7eb5642932bd53430
J2wice <jjohnstongames@gmail.com>
2017-01-21 08:06:36 -0700

    Updated UI scenes and models and such. Lots of things

---
+++ Assets/UI/Scripts/CornerCycleColor.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CornerCycleColor : MonoBehaviour {
+
+	private Color[] colors;
+	private int currentColor;
+	private bool cyclingForward;
+	private float lerper;
+	private float cycleSpeed;
+	private Image corner;
+	public Text scoreText;
+	public ParticleSystem particlesH;
+	public ParticleSystem particlesV;
+
+	public void Awake()
+	{
+		corner = this.GetComponent<Image> ();
+	}
+
+	public void Initialize(Color[] initial, float speed)
+	{
+		cycleSpeed = speed;
+		SwitchColor (initial);
+		DisableParticles ();
+	}
+
+	public void DisableParticles()
+	{
+		particlesH.Stop ();
+		particlesV.Stop ();
+	}
+
+	public void EnableParticles()
+	{
+		particlesH.Play ();
+		particlesV.Play ();
+	}
+
+
+	public void Update()
+	{
+		lerper += cycleSpeed * Time.deltaTime;
+		if (cyclingForward == true) {
+			Color temp = Color.Lerp(colors [currentColor], colors[currentColor + 1], lerper);
+			scoreText.color = temp;
+			corner.color = temp;
+			var col = particlesH.main;
+			col.startColor = temp;
+			col = particlesV.main;
+			col.startColor = temp;
+			if (lerper>=1) {
+				if (currentColor == colors.Length - 2) {
+					cyclingForward = false;
+				}
+				currentColor += 1;
+				lerper=0;
+			}
+		} else {
+			Color temp = Color.Lerp (colors [currentColor], colors[currentColor - 1], lerper);
+			scoreText.color = temp;
+			corner.color = temp;
+			var col = particlesH.main;
+			col.startColor = temp;
+			col = particlesV.main;
+			col.startColor  = temp;
+			if (lerper>=1) {
+				if (currentColor == 1) {
+					cyclingForward = true;
+				}
+				currentColor -= 1;
+				lerper=0;
+			}
+		}
+	}
+
+	public void Score(int score)
+	{
+		scoreText.text = score.ToString ();
+	}
+
+	public void SwitchColor(Color[] newList)
+	{
+		colors = newList;
+		currentColor = 0;
+		cyclingForward = true;
+	}
+
+}
---
+++ Assets/UI/Scripts/GameUI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+	public CornerCycleColor[] corners;
+	public Text timer;
+
+	//Sequences for cycling
+	public Color[] red;
+	public Color[] green;
+	public Color[] blue;
+	public Color[] yellow;
+	public float cycleSpeed;
+
+	//Images for updating the UI
+	public void Awake()
+	{
+		corners [0].Initialize (red, cycleSpeed);
+		corners [1].Initialize (green, cycleSpeed);
+		corners [2].Initialize (blue, cycleSpeed);
+		corners [3].Initialize (yellow, cycleSpeed);
+	}
+
+	public void UpdateColor(int player, GameManager.PlayerColor color)
+	{
+		Color[] temp = red;
+		switch (color) {
+		case GameManager.PlayerColor.Red:
+			temp = red;
+			break;
+		case GameManager.PlayerColor.Green:
+			temp = green;
+			break;
+		case GameManager.PlayerColor.Blue:
+			temp = blue;
+			break;
+		case GameManager.PlayerColor.Yellow:
+			temp = yellow;
+			break;
+		}
+		corners [player-1].SwitchColor (temp);
+	}
+
+	public void SetCurrentLeader(int player)
+	{
+		for (int i = 0; i < corners.Length; i++) {
+			if (i == player - 1) {
+				corners [i].EnableParticles ();
+			} else {
+				corners [i].DisableParticles ();
+			}
+		}
+	}
+
+	public void UpdateScore(int score, int player)
+	{
+		corners [player - 1].Score (score);
+	}
+
+	public void UpdateTimer(float time)
+	{
+		timer.text = string.Format("{0}", (double)time);
+	}
+	
+
+}
