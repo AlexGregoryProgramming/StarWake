91e844a5f9f222a869f36bd01653086644786a91
Marcus R. Brown <me@igetgam.es>
2017-01-22 11:49:56 -0700

    Add a Wall component.

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Wall : MonoBehaviour {
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+}
