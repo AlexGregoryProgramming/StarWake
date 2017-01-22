7c9ae4b70c3fe3d0ac2c4746c3bf6d723a73e038
Marcus R. Brown <me@igetgam.es>
2017-01-21 20:44:30 -0700

    Check for an attached VectorGrid in DirectionalGridForce.

--- Assets/Scripts/DirectionalGridForce.cs
+++ Assets/Scripts/DirectionalGridForce.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DirectionalGridForce : VectorGridForce {
 
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() {
+        if (m_VectorGrid == null) {
+            Debug.LogWarning("DirectionalGridForce should have a VectorGrid instance", this);
+            enabled = false;
+        }
+    }
+
 	// Update is called once per frame
-	void Update () 
-	{
+	void Update() {
         m_VectorGrid.AddGridForce(this.transform.position, transform.up * m_ForceScale, m_Radius, m_Color, m_HasColor);
 	}
 }
