623098970c2bf19258a2acad1742c92a28279cab
Marcus R. Brown <me@igetgam.es>
2017-01-21 17:12:00 -0700

    Add the DirectionalGridForce component.

---
+++ Assets/Scripts/DirectionalGridForce.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalGridForce : VectorGridForce {
+
+	// Update is called once per frame
+	void Update () {
+        m_VectorGrid.AddGridForce(this.transform.position, transform.up * m_ForceScale, m_Radius, m_Color, m_HasColor);
+	}
+}
