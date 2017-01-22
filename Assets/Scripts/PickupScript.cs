02ddbc5fbfc1e72cbcaa7d7d35a38521cf83f96e
AGREG124036\Alex <alegrego@uat.edu>
2017-01-21 23:01:59 -0700

    pickup code done.

--- Assets/Scripts/PickupScript.cs
+++ Assets/Scripts/PickupScript.cs
@@ -6,11 +6,14 @@ public class PickupScript : MonoBehaviour {
 	public int value;
 	public float fillTime;
 
+	public float collectionForce;
+	public float collectionRadius;
 	public Image pickUP;
+
+	public GameManager.PlayerColor collectorColor;
 	// Use this for initialization
 	void Start () 
 	{
-		pickUP = this.gameObject.GetComponent<Image> ();
 		pickUP.fillAmount = 0.0f;
 	}
 	
@@ -23,5 +26,40 @@ public class PickupScript : MonoBehaviour {
 		}
 
 
+	}
+
+	public void OnCollisionEnter(Collision col)
+	{
+		print ("A collision");
+		if (pickUP.fillAmount == 1 && col.gameObject.GetComponent<ShipColor> () != null) 
+		{
+			print ("A collision we want");
+			int playerNum = col.gameObject.GetComponent<ShipColor> ().playerNumber;
+			collectorColor = GameManager._GAMEMANAGER.GetPlayerColor (playerNum);
+
+			if (collectorColor == GameManager.PlayerColor.Blue) 
+			{
+				GameManager._GAMEMANAGER.bluePointsScored (value);
+			}
+
+			if (collectorColor == GameManager.PlayerColor.Green) 
+			{
+				GameManager._GAMEMANAGER.greenPointsScored (value);
+			}
+
+			if (collectorColor == GameManager.PlayerColor.Red) 
+			{
+				GameManager._GAMEMANAGER.redPointsScored (value);
+			}
+
+			if (collectorColor == GameManager.PlayerColor.Yellow) 
+			{
+				GameManager._GAMEMANAGER.yellowPointsScored (value);
+			}
+
+			print (collectorColor);
+			Destroy(this.gameObject);
+		
+		}
 	}
 }
