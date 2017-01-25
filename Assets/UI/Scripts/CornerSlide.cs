118715e25aa9510921e50e685604557d71ba6d11
J2wice <jjohnstongames@gmail.com>
2017-01-24 21:50:09 -0700

    Updated initial UI

---
+++ Assets/UI/Scripts/CornerSlide.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CornerSlide : MonoBehaviour {
+
+	RectTransform mytransform;
+	public Vector3 endCoordinates;
+	public Image centerTransparency;
+	public Image cornerTransparency;
+	public Image Abutton;
+	public Text scoreText;
+	public Text playerText;
+
+	bool center;
+	Color centerColor;
+	Vector3 startCoordinates;
+	float progress;
+	float accel;
+	bool start = true;
+
+
+	public void Awake()
+	{
+		mytransform = this.GetComponent<RectTransform> ();
+		startCoordinates = new Vector3 (mytransform.anchoredPosition3D.x, mytransform.anchoredPosition3D.y, mytransform.anchoredPosition3D.z);
+		Color invis = new Color (1, 1, 1, 0);
+		playerText.color = invis;
+		scoreText.color = invis;
+		cornerTransparency.color = invis;
+		centerColor = centerTransparency.color;
+		Abutton.color = invis;
+		centerTransparency.color = invis;
+		center = true;
+		progress = 0;
+	}
+
+	public void Update()
+	{
+		if (start == false) {
+			if (center == false) {
+				accel = progress / 2;
+				progress += (accel + (Time.deltaTime * 0.5f));
+				if (progress >= 1) {
+					progress = 1;
+				}
+			} else {
+				progress -= Time.deltaTime * 2;
+				if (progress <= 0) {
+					progress = 0;
+				}
+			}
+			mytransform.anchoredPosition3D = Vector3.Lerp (startCoordinates, endCoordinates, progress);
+			playerText.color = Color.Lerp (new Color (1, 1, 1, 0), Color.white, progress);
+			scoreText.color = Color.Lerp (new Color (1, 1, 1, 0), Color.white, progress);
+			centerTransparency.color = Color.Lerp (centerColor, Color.black, progress);
+			Abutton.color = Color.Lerp (new Color (1, 1, 1, 0.35f), new Color (1, 1, 1, 0) , progress);
+		} else {
+			progress += Time.deltaTime;
+			centerTransparency.color = Color.Lerp (new Color (1,1,1,0), centerColor, progress);
+			cornerTransparency.color = Color.Lerp (new Color (1,1,1,0), Color.white, progress);
+			Abutton.color = Color.Lerp (new Color (1, 1, 1, 0), new Color (1, 1, 1, 0.35f), progress);
+			if (progress >= 1) {
+				start = false;
+				progress = 0;
+				Center ();
+			}
+		}
+	}
+
+	public void FadeIn()
+	{
+		progress = 0;
+		start = true;
+	}
+
+	public void Corner()
+	{
+		center = false;
+		accel = 0;
+	}
+
+	public void Center()
+	{
+		center = true;
+	}
+}
--- Assets/UI/Scripts/TitleGameScripting.cs
+++ Assets/UI/Scripts/TitleGameScripting.cs
@@ -22,21 +22,10 @@ public class TitleGameScripting : MonoBehaviour {
 	public MainMenuStates mystate;
 
 	//Second phase
-	public Sprite lit;
-	public Sprite unlit;
-	public GameObject upperRight;
-	private Vector3 finalPosUpperRight;
-	public Image upperRightCorner;
-	public GameObject upperLeft;
-	private Vector3 finalPosUpperLeft;
-	public Image upperLeftCorner;
-	public GameObject lowerLeft;
-	private Vector3 finalPosLowerLeft;
-	public Image lowerLeftCorner;
-	public GameObject lowerRight;
-	private Vector3 finalPosLowerRight;
-	public Image lowerRightCorner;
+	public CornerSlide[] corners;
 	public GameObject diamond;
+
+	//Third phase
 	public GameObject roundSelecter;
 	public GameObject tutorial;
 	public Image[] tutorialScreens;
@@ -65,15 +54,10 @@ public class TitleGameScripting : MonoBehaviour {
 
 	public void Awake()
 	{
-		finalPosUpperRight = upperRight.transform.localPosition;
-		finalPosLowerRight = lowerRight.transform.localPosition;
-		finalPosLowerLeft = lowerLeft.transform.localPosition;
-		finalPosUpperLeft = upperLeft.transform.localPosition;
-		lowerRight.transform.localPosition= new Vector3(finalPosLowerRight.x+1000, finalPosLowerRight.y-1000, finalPosLowerRight.z);
-		lowerLeft.transform.localPosition= new Vector3(finalPosLowerLeft.x-1000, finalPosLowerLeft.y-1000, finalPosLowerLeft.z);
-		upperLeft.transform.localPosition= new Vector3(finalPosUpperLeft.x-1000, finalPosUpperLeft.y+1000, finalPosUpperLeft.z);
-		upperRight.transform.localPosition= new Vector3(finalPosUpperRight.x+1000, finalPosUpperRight.y+1000, finalPosUpperRight.z);
 		diamond.SetActive (false);
+		foreach (CornerSlide corner in corners) {
+			corner.gameObject.SetActive (false);
+		}
 	}
 
 	public void Update()
@@ -138,48 +122,37 @@ public class TitleGameScripting : MonoBehaviour {
 			delay -= Time.deltaTime*1000;
 			if (delay <=0) {
 				delay = 0;
-			}
-			lowerRight.transform.localPosition= new Vector3(finalPosLowerRight.x+delay, finalPosLowerRight.y-delay, finalPosLowerRight.z);
-			lowerLeft.transform.localPosition= new Vector3(finalPosLowerLeft.x-delay, finalPosLowerLeft.y-delay, finalPosLowerLeft.z);
-			upperLeft.transform.localPosition= new Vector3(finalPosUpperLeft.x-delay, finalPosUpperLeft.y+delay, finalPosUpperLeft.z);
-			upperRight.transform.localPosition= new Vector3(finalPosUpperRight.x+delay, finalPosUpperRight.y+delay, finalPosUpperRight.z);
-			if (delay == 0) {
-				diamond.gameObject.SetActive (true);
 				mystate = MainMenuStates.playersAbleToJoin;
-				upperLeftCorner.color = Color.red;
-				upperRightCorner.color = Color.red;
-				lowerLeftCorner.color = Color.red;
-				lowerRightCorner.color = Color.red;
+				diamond.SetActive (true);
+				foreach (CornerSlide corner in corners) {
+					corner.FadeIn ();
+					corner.gameObject.SetActive (true);
+				}
 			}
 		} else if (mystate == MainMenuStates.playersAbleToJoin) {
 			//If player one presses A, turn on green sprite upper left. upperLeftCorner.sprite = lit;
 			//Player presses start to go to next screen.
-
 			if(Input.GetKeyDown (KeyCode.Joystick1Button0))
 			{
-				upperLeftCorner.sprite = lit;
-				upperLeftCorner.color = Color.green;
+				corners [0].Corner ();
 				GameManager._GAMEMANAGER.p1Joined = true;
 			}
 
 			if(Input.GetKeyDown (KeyCode.Joystick2Button0))
 			{
-				upperRightCorner.sprite = lit;
-				upperRightCorner.color = Color.green;
+				corners [1].Corner ();
 				GameManager._GAMEMANAGER.p2Joined = true;
 			}
 
 			if(Input.GetKeyDown (KeyCode.Joystick3Button0))
 			{
-				lowerLeftCorner.sprite = lit;
-				lowerLeftCorner.color = Color.green;
+				corners [2].Corner ();
 				GameManager._GAMEMANAGER.p3Joined = true;
 			}
 
 			if(Input.GetKeyDown (KeyCode.Joystick4Button0))
 			{
-				lowerRightCorner.sprite = lit;
-				lowerRightCorner.color = Color.green;
+				corners [3].Corner ();
 				GameManager._GAMEMANAGER.p4Joined = true;
 			}
 
@@ -187,30 +160,25 @@ public class TitleGameScripting : MonoBehaviour {
 
 			if(Input.GetKeyDown (KeyCode.Joystick1Button1))
 			{
-				upperLeftCorner.sprite = unlit;
-				upperLeftCorner.color = Color.red;
+				corners [0].Center ();
 				GameManager._GAMEMANAGER.p1Joined = false;
 			}
 
 			if(Input.GetKeyDown (KeyCode.Joystick2Button1))
 			{
-				upperRightCorner.sprite = unlit;
-				upperRightCorner.color = Color.red;
-
+				corners [1].Center ();
 				GameManager._GAMEMANAGER.p2Joined = false;
 			}
 
 			if(Input.GetKeyDown (KeyCode.Joystick3Button1))
 			{
-				lowerLeftCorner.sprite = unlit;
-				lowerLeftCorner.color = Color.red;
+				corners [2].Center ();
 				GameManager._GAMEMANAGER.p3Joined = false;
 			}
 
 			if(Input.GetKeyDown (KeyCode.Joystick4Button1))
 			{
-				lowerRightCorner.sprite = unlit;
-				lowerRightCorner.color = Color.red;
+				corners [3].Center ();
 				GameManager._GAMEMANAGER.p4Joined = false;
 			}
 
@@ -302,10 +270,6 @@ public class TitleGameScripting : MonoBehaviour {
 
 	public void AttachAllObjectsToParentPart2()
 	{
-		upperLeft.gameObject.transform.parent = parentForMovement;
-		lowerLeft.gameObject.transform.parent = parentForMovement;
-		lowerRight.gameObject.transform.parent = parentForMovement;
-		upperRight.gameObject.transform.parent = parentForMovement;
 		diamond.gameObject.transform.parent = parentForMovement;
 		mystate = MainMenuStates.moveEverythingDown2;
 	}
