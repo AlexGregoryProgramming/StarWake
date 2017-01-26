e3506ea898a2ebaac70df92cd0b2f4895e3e870b
Marcus R. Brown <me@igetgam.es>
2017-01-25 20:20:56 -0700

    Add InputManager class.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+﻿#pragma warning disable 0162 // Unreachable code detected
+
+using UnityEngine;
+
+public sealed class InputManager : MonoBehaviour {
+
+    public static bool IsStartPressed {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetButton("StartOSX");
+            }
+
+            return Input.GetButton("Start");
+        }
+    }
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
