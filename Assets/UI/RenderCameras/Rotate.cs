2d42c9ced7c154c74d281f22927469ebc5994220
J2wice <jjohnstongames@gmail.com>
2017-01-26 00:16:04 -0700

    Added new ships and new screen for new ship selection

---
+++ Assets/UI/RenderCameras/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public float speed;
+	public GameObject rotObject;
+
+	// Update is called once per frame
+	void Update () {
+		Quaternion currentRot = rotObject.transform.localRotation;
+		Quaternion modifier = Quaternion.Euler (0, speed * Time.deltaTime, 0);
+		currentRot *= modifier;
+		rotObject.transform.localRotation = currentRot;
+	}
+}
