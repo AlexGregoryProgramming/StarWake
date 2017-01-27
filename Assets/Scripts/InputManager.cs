8dfaf6b74860af758cef274f7bd446b5a9054b76
Marcus R. Brown <me@igetgam.es>
2017-01-26 20:25:50 -0700

    InputManager: Add properties to query the state of each face button on all 4 controllers.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -44,6 +44,166 @@ public sealed class InputManager : MonoBehaviour {
         }
     }
 
+    public static bool IsP1GreenDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick1Button16);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick1Button0);
+        }
+    }
+
+    public static bool IsP2GreenDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick2Button16);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick2Button0);
+        }
+    }
+
+    public static bool IsP3GreenDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick3Button16);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick3Button0);
+        }
+    }
+
+    public static bool IsP4GreenDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick4Button16);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick4Button0);
+        }
+    }
+
+    public static bool IsP1RedDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick1Button17);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick1Button1);
+        }
+    }
+
+    public static bool IsP2RedDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick2Button17);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick2Button1);
+        }
+    }
+
+    public static bool IsP3RedDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick3Button17);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick3Button1);
+        }
+    }
+
+    public static bool IsP4RedDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick4Button17);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick4Button1);
+        }
+    }
+
+    public static bool IsP1BlueDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick1Button18);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick1Button2);
+        }
+    }
+
+    public static bool IsP2BlueDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick2Button18);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick2Button2);
+        }
+    }
+
+    public static bool IsP3BlueDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick3Button18);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick3Button2);
+        }
+    }
+
+    public static bool IsP4BlueDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick4Button18);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick4Button2);
+        }
+    }
+
+    public static bool IsP1YellowDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick1Button19);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick1Button3);
+        }
+    }
+
+    public static bool IsP2YellowDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick2Button19);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick2Button3);
+        }
+    }
+
+    public static bool IsP3YellowDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick3Button19);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick3Button3);
+        }
+    }
+
+    public static bool IsP4YellowDown {
+        get {
+            if (Platform.IsOSX) {
+                return Input.GetKeyDown(KeyCode.Joystick4Button19);
+            }
+
+            return Input.GetKeyDown(KeyCode.Joystick4Button3);
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
 
